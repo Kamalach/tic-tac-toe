ed702e49b06243359ad72dc3869a16588e235b4d
kamalach <kamal.acharya608@gmail.com>
2025-01-21 06:10:58 +0545

    add game setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TicTacToe from './Components/TicTacToe/TicTacToe';
+// import { IoIosRadioButtonOff } from "react-icons/io";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
